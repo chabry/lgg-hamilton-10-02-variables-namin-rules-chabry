8e4bfc3740ff9ba2e46dab4fa1b8f4a86b0c8e76
Chabry <chabry01@gmail.com>
2024-11-21 16:15:19 +0100

    Exercice résolu

--- namingRules.js
+++ namingRules.js
@@ -3,26 +3,35 @@
 // Function 1
 function validVariableName() {
   // Your code here
+  let validVariableName
+  return validVariableName
 }
 
 // Function 2
 function invalidVariableName() {
   // Your code here
+  return ZOULOU = "123"
 }
 
 // Function 3
 function camelCaseVariableName() {
   // Your code here
+  let camelCaseVariableName
+  return camelCaseVariableName
 }
 
 // Function 4
 function snake_caseVariableName() {
   // Your code here
+  let snake_case_variable_name
+  return snake_case_variable_name
 }
 
 // Function 5
 function PascalCaseVariableName() {
   // Your code here
+  let PascalCaseVariableName
+  return PascalCaseVariableName
 }
 
 module.exports = {
